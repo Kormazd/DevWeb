ddbc5749fda2b1c38458fed3e8e1f8483c4eeb54
TomWmbk <tom.wambeke23@gmail.com>
2025-09-09 16:10:48 +0200

    api flask Hello World

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
@@ -9,4 +11,5 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run(port=5001)
+
 
