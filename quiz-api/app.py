aa8657ac1c7369c76a22b3bf4bbc023681906fcd
TomWmbk <tom.wambeke23@gmail.com>
2025-09-10 16:20:09 +0200

    admin password

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,10 +1,29 @@
 from flask import Flask
 from flask_cors import CORS
+from flask import Flask, request
+import hashlib
+import secrets
+
+ADMIN_PASSWORD = 'd278077bbfe7285a144d4b5b11adb9cf'
+
+def build_token():
+    return secrets.token_urlsafe(32)
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+@app.route('/login', methods=['POST'])
+def login():
+	payload = request.get_json()
+	if not payload or 'password' not in payload:
+		return 'Unauthorized', 401
+	provided_password: str = str(payload.get('password', ''))
+	provided_hash = hashlib.md5(provided_password.encode()).hexdigest()
+	if provided_hash != ADMIN_PASSWORD:
+		return 'Unauthorized', 401
+	token = build_token()
+	return {'token': token}, 200
+
 def hello_world():
 	x = 'world'
 	return f"Hello, {x}"
